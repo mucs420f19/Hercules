6f19a491d57bdae5cfe6fddce35a82177e7e3ef2
Silviu <silviuburz@yahoo.com>
2019-10-17 12:50:00 -0400

    Auto stash before merge of "s2-gui_implementation" and "origin/s2-gui_implementation"

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brian/Hercules
-# Build directory: /home/brian/Hercules/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(mytest "HerculesTest")
