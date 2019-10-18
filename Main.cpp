8a80488fa4f40bb25eac638dff7a78b6e4c6aacf
Brian <bmekstro@millersville.edu>
2019-10-18 14:44:51 -0400

    Working functionality for adding fields and methods to GUI.

--- Main.cpp
+++ Main.cpp
@@ -46,6 +46,9 @@ int main(int argc, char** argv)
 	
 	//Vars for class holders
 	UMLObjectsHolder* holder = new UMLObjectsHolder();
+	bool run = true;
+	int choice;
+	std::string input, input2;
 	
 	if (argc == 1)
 	{
@@ -59,7 +62,7 @@ int main(int argc, char** argv)
 		char del[256] = {0};
 		char save[256] = {0};
 		char load[256] = {0};
-		std::string method;
+		char method[20] = {0};
 		char field [256] = {0};
 		static int show_menu = nk_true;
 		static int show_app_about = nk_false;
@@ -148,17 +151,6 @@ int main(int argc, char** argv)
 						(holder->DeleteUMLObject(del));
 
 		}
-
-		/*
-			nk_layout_row_static(ctx, 0, 100, 2);
-
-			nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, method, sizeof(method) - 1, nk_filter_default);
-					if (nk_button_label(ctx, "Add Method"))		
-					printf("test");									
-			nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, field, sizeof(field) - 1, nk_filter_default);
-					if (nk_button_label(ctx, "Add Field"))						
-						printf("test");
-		*/
 		
 		//Creates dropdown box that lists all created classes.  Updates dynamically when a class is added or deleted.			
 		nk_layout_row_begin(ctx, NK_STATIC, 25, 5);
@@ -172,7 +164,8 @@ int main(int argc, char** argv)
 					}
 				nk_menu_end(ctx);
 			}
-/*
+
+			//Right now this has no input validation.  Allows multiple methods of same name to be created.
 		nk_layout_row_begin(ctx, NK_STATIC, 25, 5);
 		nk_layout_row_push(ctx, 200);
 		if (nk_menu_begin_label(ctx, "Add method", NK_TEXT_LEFT, nk_vec2(500, 200)))
@@ -182,17 +175,34 @@ int main(int argc, char** argv)
 					if (nk_button_label(ctx, "Confirm Class Name"))	
 						printf("te");
 				nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, method, sizeof(method) - 1, nk_filter_default);
-					if (nk_button_label(ctx, "Enter Method Name"))
+					if (nk_button_label(ctx, "Enter Method Name")) {
 						printf("Method Name");
+						UMLMethod newMethod;
+						newMethod.SetName(method);
+						holder->GetUMLObject("cat")->AddMethod(newMethod);
+					}
 
-					UMLMethod newMethod;
-					newMethod.SetName(method);
-
-					holder->GetUMLObject(add)->AddMethod(method);
 
 				nk_menu_end(ctx);
-			}*/
+			}
+			//Right now this has no input validation.  Allows multiple fields of same name to be created.
+		if (nk_menu_begin_label(ctx, "Add field", NK_TEXT_LEFT, nk_vec2(500, 200)))
+			{
+				nk_layout_row_static(ctx, 0, 100, 2);
+				nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, add, sizeof(add) - 1, nk_filter_default);
+					if (nk_button_label(ctx, "Confirm Class Name"))	
+						printf("te");
+				nk_edit_string_zero_terminated(ctx, NK_EDIT_FIELD, field, sizeof(field) - 1, nk_filter_default);
+					if (nk_button_label(ctx, "Enter field Name")) {
+						printf("Field Name");
+						UMLField newField;
+						newField.SetName(field);
+						holder->GetUMLObject("cat")->AddField(newField);
+					}
 		
+
+				nk_menu_end(ctx);
+			}
 			//Close the ctx struct context
 			nk_end(ctx);
 
@@ -212,10 +222,181 @@ int main(int argc, char** argv)
 		nk_glfw3_shutdown();
 		glfwTerminate();
 
-
 }
+
 	else
-	{RunREPL(holder);}
-	
+	{
+		std::cout << std::endl << "********************Welcome to Hercules********************" << std::endl;
+
+		while (run)
+		{
+			std::cout << std::endl << "********************" << std::endl;
+			std::cout << "1. View Class List" << std::endl;
+			std::cout << "2. Create New Class" << std::endl;
+			std::cout << "3. Delete A Class" << std::endl;
+			std::cout << "4. Edit A Class" << std::endl;
+			std::cout << "5. Save" << std::endl;
+			std::cout << "6. Load" << std::endl;
+			std::cout << "7. Exit" << std::endl;
+			std::cout << "********************" << std::endl << std::endl;
+			std::cout << "Which task would you like to run?" << std::endl;
+
+			std::cin >> choice;
+
+			switch (choice)
+			{
+			case 1:
+			{
+				std::cout << std::endl << "Class List:" << std::endl;
+
+				holder->UMLObjectPrintContents();
+
+				break;
+			}
+
+			case 2:
+			{
+				std::cout << std::endl << "Please enter the name of the new class." << std::endl;
+				std::cin >> input;
+				std::cout << std::endl;
+
+				if (holder->CreateNewClass(input))
+					std::cout << "Class created sucessfully." << std::endl;
+				else
+					std::cout << "Class creation failed." << std::endl;
+
+				break;
+			}
+
+			case 3:
+			{
+				if (holder->Size())
+				{
+					std::cout << std::endl;
+
+					holder->UMLObjectPrintTitles();
+
+					std::cout << std::endl << "Please enter the number of the class you wish to delete." << std::endl;
+					std::cin >> input;
+
+					if (holder->DeleteUMLObject(input))
+						std::cout << "Delete successful." << std::endl << std::endl;
+					else
+						std::cout << "Unable to delete class." << std::endl << std::endl;
+				}
+				else
+					std::cout << std::endl << "You have no classes to delete." << std::endl;
+
+				break;
+			}
+
+			case 4:
+			{
+				std::cout << std::endl << "Please enter the name of the class you wish to edit." << std::endl;
+
+				std::cin >> input;
+
+				std::cout << std::endl << "Please enter the new name for the class." << std::endl;
+
+				std::cin >> input2;
+
+				if (holder->EditClassTitle(input2, input))
+					std::cout << "Rename successful." << std::endl << std::endl;
+				else
+					std::cout << "Unable to rename class." << std::endl << std::endl;
+
+				break;
+			}
+
+			case 5:
+			{
+				std::cout << std::endl << "Please enter the name of the save file." << std::endl;
+
+				std::cin >> input;
+
+				if (SavingLoadingIO::SaveProjectToFile(holder, input) == SaveAlreadyExists)
+				{
+					std::cout << std::endl << "Save already exists. Overwrite? y/n: " << std::endl;
+					std::cin >> input;
+					if (input == "y")
+					{
+						SavingLoadingIO::SaveProjectToFile(holder, input, true); //for force overwrite
+					}
+					else
+					{
+						std::cout << std::endl << "Save cancelled" << std::endl;
+					}
+				}
+
+				break;
+			}
+
+			case 6:
+			{
+				std::cout << std::endl << "Please enter the name of the file you wish to load." << std::endl;
+
+				std::cin >> input;
+
+				//add something in here to ask if they want to save the current project... otherwise it gets thrown out
+				if (SavingLoadingIO::LoadProject(holder, input))
+					std::cout << "Load successful." << std::endl << std::endl;
+				else
+					std::cout << "Unable to load." << std::endl << std::endl;
+
+				break;
+			}
+
+			case 7:
+			{
+				run = false;
+
+				std::cout << std::endl << "********************" << std::endl;
+				std::cout << "Thank you for using Hercules!" << std::endl;
+				std::cout << "********************" << std::endl << std::endl;
+
+				break;
+			}
+			case 8:
+			{
+				std::cout << std::endl << "Please enter the name of the class you wish to edit." << std::endl;
+
+				std::cin >> input;
+
+				std::cout << std::endl << "Please enter the method name." << std::endl;
+
+				std::cin >> input2;
+
+					UMLMethod newMethod;
+					newMethod.SetName(input2);
+
+					holder->GetUMLObject(input)->AddMethod(newMethod);
+
+				break;
+			}
+			case 9:
+			{
+				std::cout << std::endl << "Please enter the name of the class you wish to edit." << std::endl;
+
+				std::cin >> input;
+
+				std::cout << std::endl << "Please enter the field name." << std::endl;
+
+				std::cin >> input2;
+
+					UMLField newField;
+					newField.SetName(input2);
+
+					holder->GetUMLObject(input)->AddField(newField);
+
+				break;
+			}
+
+			default:
+			{
+				std::cout << std::endl << "Invalid entry, please enter a number from 1 to 8." << std::endl << std::endl;
+			}
+			}
+		}
+	}
 	return 0;
 }
