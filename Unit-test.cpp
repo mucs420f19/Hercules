29794e1c49b9206348bdad38b15e49b868c840a4
Quinn Irons <qpirons@gmail.com>
2019-10-21 13:57:55 -0400

    Moved Commented tests

--- Unit-test.cpp
+++ Unit-test.cpp
@@ -44,7 +44,7 @@ TEST_CASE("Edit a class", "0")
 
 }
 
-
+/*
 TEST_CASE("Add multiple classes", "0")
 {
 	UMLObjectsHolder* holder = new UMLObjectsHolder();
@@ -61,7 +61,7 @@ TEST_CASE("Add multiple classes", "0")
 		REQUIRE(holder->UMLObjectReturnTitlesString()[1] == "Tire");
 	}
 }
-/*
+*/
 TEST_CASE("Add relationship between classes", "0")
 {
 	UMLObjectsHolder* holder = new UMLObjectsHolder();
@@ -85,7 +85,7 @@ TEST_CASE("Add relationship between classes", "0")
 	}
 
 }
-*/
+
 TEST_CASE("Test Saving Loading All Items", "0")
 {
 	UMLObjectsHolder* holder = new UMLObjectsHolder();
