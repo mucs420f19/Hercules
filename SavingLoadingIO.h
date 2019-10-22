5338a55f707f1f6510cd43e692ce2cfdc265f714
Silviu <silviuburz@yahoo.com>
2019-10-21 22:49:54 -0400

    comments

--- SavingLoadingIO.h
+++ SavingLoadingIO.h
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <algorithm>
 
-#define DefaultFileName "Output.xml"
+#define DefaultFileName "Output.yml"
 
 const int SaveSuccess = 0;
 const int SaveError = 1;
@@ -33,8 +33,18 @@ namespace SavingLoadingIO
 		std::string type;
 	};
 
+	//SaveProjectToFile does as it says. A default file name is given if there is none specified
+	//the file will return SaveSuccess for success, SaveError for an unspecified error,
+	//or SaveAlreadyExists if the target file already exists
+	//the file can be overwritten if the overwrite optional parameter is true
 	int SaveProjectToFile(UMLObjectsHolder * in, std::string filename = DefaultFileName, bool overwrite = false);
+
+	//LoadProject accepts a UMLObjectsHolder and attempts to load filename into it as a Hercules project
+	//The contents of the UMLObjectsHolder passed in are deleted regardless of what happens
+	//a default file name is given if there is none specified
 	bool LoadProject(UMLObjectsHolder* out, std::string filename = DefaultFileName);
+
+	//these functions assign in generating and parsing the file for saving/loading
 	std::string RemoveQuotes(std::string in);
 	size_t GetIndent(std::string in);
 	bool IsItem(std::string in);
@@ -44,8 +54,14 @@ namespace SavingLoadingIO
 	std::string StripItem(std::string in);
 	std::string StripNode(std::string in);
 	void ParseNode(Node* current, std::vector<std::string> lines, size_t c = 0);
-	void ProcessResults(Node* current, UMLObjectsHolder* out, std::vector<Relationship> &relationships);
 	std::string FindChildWhere(Node* in, std::string key);
 	std::vector<Node *> FindNodesWhere(Node * in, std::string key);
+
+	//clean up all resources that were necessary for generating the UMLObjectsHolder during loading
 	void LoadingCleanup(Node* in);
+
+	//Once the items have been read from the file and a tree has been constructed, this function is used to load
+	//the contents into a UMLObjectsHolder by parsing the tree
+	void ProcessResults(Node* current, UMLObjectsHolder* out, std::vector<Relationship>& relationships);
+
 }
