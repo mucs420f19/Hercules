aaf2b8918a69d4b0e0bf9336de732e0472181910
lawilli4 <54599790+lawilli4@users.noreply.github.com>
2019-11-19 11:56:11 -0500

    Created 'Class.cpp' and its functions
    Created the source file to match Professor Killian's 'Class.h' header file. Also added almost all functions introduced in the Class header file. Still missing 'rawName()'.

---
+++ Class.cpp
@@ -0,0 +1,143 @@
+#include "Class.h"
+
+// Default constructor
+Class::Class()
+{}
+
+// Constructor given a name
+Class::Class(const std::string &name)
+{
+    mName = name;
+}
+
+// Changes the name of an existing class to a new given name
+void Class::rename(const std::string &newName)
+{
+    mName = newName;
+}
+
+// Returns the name of a class
+const std::string Class::&name() const
+{
+    return mName;
+}
+
+// ==================================================
+const char *Class::rawName() const
+{
+
+}
+
+// Adds a field to the classes' field vector
+void Class::addField(const Field& field)
+{
+    mFields.push_back(field);
+}
+
+// Adds a method to the classes' method vector
+void Class::addMethod(const Method& method)
+{
+    mMethods.push_back(method);
+}
+
+// Removes a field from the vector of fields at the given index
+void Class::removeField(int index)
+{
+    mFields.erase(index);
+}
+
+// Removes a field from the vector of fields after finding its index
+void Class::removeField(const std::string& name)
+{
+    unsigned int index = 0;
+	for (auto i : mFields)
+	{
+		if (i.name() == name)
+			mFields.removeField(index);
+		
+        ++index;
+	}
+}
+
+// Removes a method from the vector of methods at the given index
+void Class::removeMethod(int index)
+{
+    mMethods.erase(index);
+}
+
+// Removes a method from the vector of methods after finding its index
+void Class::removeMethod(const std::string& name)
+{
+    unsigned int index = 0;
+	for (auto i : mMethods)
+	{
+		if (i.name() == name)
+			mMethods.removeMethod(index);
+		
+        ++index;
+	}
+}
+
+// Returns a method from the vector of methods given its name
+Method& Class::method(const std::string& name)
+{
+    for (auto i : mMethods)
+    {
+        if (i.name() == name)
+            return i;
+    }
+}
+
+// Returns a method from the vector of methods given its name
+const Method& Class::method(const std::string& name) const
+{
+    for (auto i : mMethods)
+    {
+        if (i.name() == name)
+            return i;
+    }
+}
+
+// Returns a method from the vector of methods given its index
+Method& Class::method(int index)
+{
+    return mMethods.at(index);
+}
+
+// Returns a method from the vector of methods given its index
+const Method& Class::method(int index) const
+{
+    return mMethods.at(index);
+}
+
+// Returns a field from the vector of fields gien its name
+Field& Class::field(const std::string& name)
+{
+    for (auto i : mFields)
+    {
+        if (i.name() == name)
+            return i;
+    }
+}
+
+// Returns a field from the vector of fields gien its name
+const Field& Class::field(const std::string& name) const
+{
+    for (auto i : mFields)
+    {
+        if (i.name() == name)
+            return i;
+    }
+}
+
+// Returns a field from the vector of fields gien its index
+Field& Class::field(int index)
+{
+    return mFields.at(index);
+}
+
+// Returns a field from the vector of fields gien its index
+const Field& Class::field(int index) const
+{
+    return mFields.at(index);
+}
