cde42a579b30304b820ff7004c12374fcf4be1c5
Quinn Irons <qpirons@gmail.com>
2019-11-21 09:08:00 -0500

    Included CommandFactory to work on compiler errors

--- CommandFactory.cpp
+++ CommandFactory.cpp
@@ -9,34 +9,34 @@ using namespace std::string_literals;
 static const std::unordered_map<std::string, int> PARAM_COUNTS{{
 
     // core
-    {"help"s, 1},
-    {"list"s, 1},
-    {"exit"s, 1},
-    {"save"s, 2},
-    {"load"s, 2},
+    {"help", 1},
+    {"list", 1},
+    {"exit", 1},
+    {"save", 2},
+    {"load", 2},
     // add
-    {"add_class"s, 2},
-    {"add_field"s, 5},
-    {"add_method"s, 6},
-    {"add_relationship"s, 4}
+    {"add_class", 2},
+    {"add_field", 5},
+    {"add_method", 6},
+    {"add_relationship", 4},
 
     // edit_class <className> <newName>
-    {"edit_class"s, 3},
+    {"edit_class", 3},
     // edit_field <attr> <className> <fieldName> <newValue>
-    {"edit_field"s, 5},
+    {"edit_field", 5},
     // edit_method <attr> <className> <methodName> <newValue>
-    {"edit_method"s, 5},
+    {"edit_method", 5},
     // edit_relationship <class1> <class2> <newType>
-    {"edit_relationship"s, 4},
+    {"edit_relationship", 4},
 
     // delete_class <name>
-    {"delete_class"s, 2},
+    {"delete_class", 2},
     // delete_field <class> <name>
-    {"delete_field"s, 3},
+    {"delete_field", 3},
     // delete_method <class> <name>
-    {"delete_method"s, 3},
+    {"delete_method", 3},
     // delete_relationship <class1> <class2>
-    {"delete_relationship"s, 3}}};
+    {"delete_relationship", 3}}};
 
 bool checkParameterCount(const std::vector<std::string> &cmdLine) {
   if (cmdLine.empty())
@@ -53,7 +53,7 @@ bool checkParameterCount(const std::vector<std::string> &cmdLine) {
 std::unique_ptr<Command>
 make_command(std::ostream& os, const std::vector<std::string> &cmdLineArgs) {
   const std::string &cmdName = cmdLineArgs.front();
-  if (!checkParameterCount(cmdLine)) {
+  if (!checkParameterCount(cmdLineArgs)) {
     return make_unique<ErrorCommand>(os, "invalid parameter count");
   }
   if (cmdName == "add_class"s) {
