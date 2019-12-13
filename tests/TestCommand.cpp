67a75160eb5f60dbcf460992633d4b8a3316206e
Quinn Irons <qpirons@gmail.com>
2019-12-12 21:15:41 -0500

    Changed method of testing commands Based upon what Silviu did

--- tests/TestCommand.cpp
+++ tests/TestCommand.cpp
@@ -1,16 +1,59 @@
+#include "../ConsoleView.h"
 #include "../Command.h"
+#include "../CommandFactory.h"
 
 #include "catch2/catch.hpp"
 
+std::vector<std::string>
+split(const std::string& line)
+{
+
+	std::vector<std::string> tokenized;
+	const char* WS = "\t ";
+	auto wordStart = line.find_first_not_of(WS);
+	while (wordStart != std::string::npos)
+	{
+		auto wordEnd = line.find_first_of(WS, wordStart);
+		if (wordEnd == std::string::npos)
+		{
+			wordEnd = line.size();
+		}
+		tokenized.emplace_back(line.begin() + wordStart, line.begin() + wordEnd);
+		wordStart = line.find_first_not_of(WS, wordEnd);
+	}
+	return tokenized;
+}
+
+void SendCommand(std::string in)
+{
+	make_command(std::cout, split(in))->execute();
+}
+
 TEST_CASE("Commands work")
 {
-	Model * test = new Model;
-	Command::setModelInstance(test);
-	SECTION("Add Class Command")
+	Model* holder = new Model;
+	Command::setModelInstance(holder)
+
+	SendCommand("add_class test1");
+	SendCommand("add_class test2");
+	SendCommand("add_class test3");
+	SendCommand("add_field test1 field1 int public");
+	SendCommand("add_field test2 field2 float private");
+	SendCommand("add_field test3 field3 double protected");
+	SendCommand("add_method test1 method1 int public");
+	SendCommand("add_method test2 method2 float private");
+	SendCommand("add_method test3 method3 double protected");
+	SendCommand("add_parameter test1 method1 param1 int");
+	SendCommand("add_parameter test2 method2 param2 float");
+	SendCommand("add_parameter test3 method3 param3 double");
+	SendCommand("add_relationship test1 test2 aggregation");
+	SendCommand("add_relationship test1 test3 composition");
+	SendCommand("add_relationship test2 test3 dependency");
+
+	SECTION("Edit Class Command")
 	{
-		std::string Name("Name");
-		std::make_unique<AddClassCommand>("Name");
-		//REQUIRE(test->findClass(Name) != nullptr);
+		SendCommand("edit_class test1 test4");
+		REQUIRE(holder->findClass(test4) != nullptr);
 	}
 
 	SECTION("Attribute can have name change")
