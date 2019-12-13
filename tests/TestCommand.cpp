8a232ab49345e9b98124f2bf32789a14f539fcd2
Quinn Irons <qpirons@gmail.com>
2019-12-12 21:48:15 -0500

    renamed functions to prevent duplicate error

--- tests/TestCommand.cpp
+++ tests/TestCommand.cpp
@@ -4,43 +4,63 @@
 
 #include "catch2/catch.hpp"
 
-void SendCommand(std::string in)
+std::vector<std::string>
+split2(const std::string& line)
 {
-	make_command(std::cout, split(in))->execute();
+
+	std::vector<std::string> tokenized;
+	const char* WS = "\t ";
+	auto wordStart = line.find_first_not_of(WS);
+	while (wordStart != std::string::npos)
+	{
+		auto wordEnd = line.find_first_of(WS, wordStart);
+		if (wordEnd == std::string::npos)
+		{
+			wordEnd = line.size();
+		}
+		tokenized.emplace_back(line.begin() + wordStart, line.begin() + wordEnd);
+		wordStart = line.find_first_not_of(WS, wordEnd);
+	}
+	return tokenized;
+}
+
+void SendCommand2(std::string in)
+{
+	make_command(std::cout, split2(in))->execute();
 }
 
 TEST_CASE("Commands work")
 {
-	Model* holder = new Model;
-	Command::setModelInstance(holder);
+	Model* holder2 = new Model;
+	Command::setModelInstance(holder2);
 
-	SendCommand("add_class test1");
-	SendCommand("add_class test2");
-	SendCommand("add_class test3");
-	SendCommand("add_field test1 field1 int public");
-	SendCommand("add_field test2 field2 float private");
-	SendCommand("add_field test3 field3 double protected");
-	SendCommand("add_method test1 method1 int public");
-	SendCommand("add_method test2 method2 float private");
-	SendCommand("add_method test3 method3 double protected");
-	SendCommand("add_parameter test1 method1 param1 int");
-	SendCommand("add_parameter test2 method2 param2 float");
-	SendCommand("add_parameter test3 method3 param3 double");
-	SendCommand("add_relationship test1 test2 aggregation");
-	SendCommand("add_relationship test1 test3 composition");
-	SendCommand("add_relationship test2 test3 dependency");
+	SendCommand2("add_class test1");
+	SendCommand2("add_class test2");
+	SendCommand2("add_class test3");
+	SendCommand2("add_field test1 field1 int public");
+	SendCommand2("add_field test2 field2 float private");
+	SendCommand2("add_field test3 field3 double protected");
+	SendCommand2("add_method test1 method1 int public");
+	SendCommand2("add_method test2 method2 float private");
+	SendCommand2("add_method test3 method3 double protected");
+	SendCommand2("add_parameter test1 method1 param1 int");
+	SendCommand2("add_parameter test2 method2 param2 float");
+	SendCommand2("add_parameter test3 method3 param3 double");
+	SendCommand2("add_relationship test1 test2 aggregation");
+	SendCommand2("add_relationship test1 test3 composition");
+	SendCommand2("add_relationship test2 test3 dependency");
 
 	SECTION("Edit Class Command")
 	{
 		std::string editName("test4");
-		SendCommand("edit_class test1 test4");
-		REQUIRE(holder->findClass(editName) != nullptr);
+		SendCommand2("edit_class test1 test4");
+		REQUIRE(holder2->findClass(editName) != nullptr);
 	}
 
 	SECTION("Delete Class Command")
 	{
 		std::string deleteName("test1");
-		SendCommand("delete_class test1");
-		REQUIRE(holder->findClass(deleteName) == nullptr);
+		SendCommand2("delete_class test1");
+		REQUIRE(holder2->findClass(deleteName) == nullptr);
 	}
 }
